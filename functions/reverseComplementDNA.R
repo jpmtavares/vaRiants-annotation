01cdfbfd6aef11b14449bf71d9203ecabe43d894
Joana Tavares <jpmtav@gmail.com>
2017-04-26 17:38:25 +0100

    Create reverseComplementDNA.R
    returns reverse (and optionally, complement) DNA string

---
+++ functions/reverseComplementDNA.R
@@ -0,0 +1,13 @@
+##########################################################
+#                                                        #
+#             reverse (and complement) DNA               #
+#                                                        #
+##########################################################
+reverseDNA<-function(seq, complement=F){
+  if(complement==T){
+    split<-strsplit(as.character(chartr("TCGA", "AGCT", seq)),"")[[1]]
+    paste(rev(split),collapse = "")
+  }else{
+    chartr("TCGA", "AGCT", seq)
+  }
+}
