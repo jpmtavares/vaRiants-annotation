e8b6a82ce77f7191b04ef77233d69de9f425c373
Joana Tavares <jpmtav@gmail.com>
2017-04-27 09:39:29 +0100

    Create UMDpredictor.R
    returns UMD-predictor results for reference transcripts of a vector of genes

---
+++ functions/UMDpredictor.R
@@ -0,0 +1,20 @@
+##########################################################
+#                                                        #
+#                      UMD-predictor                     #
+#                                                        #
+##########################################################
+UMDpredictor<-function(genes){
+  transcripts<-read.delim("../sources/GRCh37ENStranscripts.txt",
+                          header = T)
+  u<-semi_join(transcripts, as.data.frame(genes), by = c("HGNC_symbol" = "genes"))
+  u$url<-paste("http://umd-predictor.eu/transcript_query2.php?name=",
+                 str_extract(u$ENSTranscript,"(?<=0{5}).*$"),sep="")
+  
+  umd<-lapply(u$url,read.delim,header = F)
+  umd<-mapply(cbind, "HGNC_symbol"=u$HGNC_symbol, "ENSTranscript"=u$ENSTranscript, umd,  SIMPLIFY=F)
+  
+  umdpredictor<-do.call("rbind", umd)
+  names(umdpredictor)[c(3:8)]<-c("TranscriptPosition","Position","HGVS_c","HGVS_p",
+                                 "score","UMD-predictor")
+  return(umdpredictor)
+}
