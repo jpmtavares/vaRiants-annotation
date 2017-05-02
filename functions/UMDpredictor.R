b08372163dadb11077a331af1155a7b2f453c459
Joana Tavares <jpmtav@gmail.com>
2017-05-02 14:11:48 +0100

    Update UMDpredictor.R
    add tryCatch to deal with transcripts that don't have information in UMD-predictor

--- functions/UMDpredictor.R
+++ functions/UMDpredictor.R
@@ -8,9 +8,15 @@ UMDpredictor<-function(genes){
                           header = T)
   u<-semi_join(transcripts, as.data.frame(genes), by = c("HGNC_symbol" = "genes"))
   u$url<-paste("http://umd-predictor.eu/transcript_query2.php?name=",
-                 str_extract(u$ENSTranscript,"(?<=0{5}).*$"),sep="")
+               str_extract(u$ENSTranscript,"(?<=0{5}).*$"),sep="")
   
-  umd<-lapply(u$url,read.delim,header = F)
+  umd<-lapply(u$url,function(x) {
+    tryCatch({read.delim(x,header = F)},
+             error=function(e){NULL})
+  })
+  u<-u[!sapply(umd, is.null),]
+  umd<-umd[!sapply(umd, is.null)]
+  
   umd<-mapply(cbind, "HGNC_symbol"=u$HGNC_symbol, "ENSTranscript"=u$ENSTranscript, umd,  SIMPLIFY=F)
   
   umdpredictor<-do.call("rbind", umd)
