763d014faec0ec1c0561920da32939d50dac058a
Joana Tavares <jpmtav@gmail.com>
2017-06-01 13:04:22 +0100

    Update vcfFORMAT.R
    add coverage formating for somatic analysis

--- functions/vcfFORMAT.R
+++ functions/vcfFORMAT.R
@@ -6,7 +6,7 @@
 # The final vcf is an ensemble of multiple variant callers
 # which results in different FORMAT fields
 
-vcfFORMAT<-function(format){
+vcfFORMAT<-function(format, somatic = FALSE){
   cov<-str_split_fixed(format, ":", 9)
   cov<-apply(cov, 2, function(x) gsub("^$|^ $", NA, x))
   cov<-as.data.frame(cov)
@@ -21,11 +21,21 @@ vcfFORMAT<-function(format){
                      "heterozygous","heterozygous")
   
   cov$genotype<-mgsub(cov_original, cov_replacement, cov[,1])
-  cov$coverage<-cov[,3]
-  cov$coverage_ref<-ifelse(cov$na_count==0, as.numeric(str_extract(cov[,4],"[^,]*")),
-                           ifelse(cov$na_count==4, as.numeric(str_extract(cov[,2],"[^,]*")), NA))
-  cov$coverage_alt<-ifelse(cov$na_count==0, as.numeric(str_extract(cov[,4],"(?<=,).*")),
-                           ifelse(cov$na_count==4, as.numeric(str_extract(cov[,2],"(?<=,).*")), NA))
+  
+  if(somatic==FALSE){
+    cov$coverage<-cov[,3]
+    cov$coverage_ref<-ifelse(cov$na_count==0, as.numeric(str_extract(cov[,4],"[^,]*")),
+                             ifelse(cov$na_count==4, as.numeric(str_extract(cov[,2],"[^,]*")), NA))
+    cov$coverage_alt<-ifelse(cov$na_count==0, as.numeric(str_extract(cov[,4],"(?<=,).*")),
+                             ifelse(cov$na_count==4, as.numeric(str_extract(cov[,2],"(?<=,).*")), NA))
+  }else{
+    cov$coverage<-ifelse(cov$na_count==2,as.numeric(as.character(cov[,2])),
+                         as.numeric(as.character(cov[,4])))
+    cov$coverage_ref<-ifelse(cov$na_count==2, as.numeric(str_extract(cov[,4],"[^,]*")),
+                             as.numeric(str_extract(cov[,2],"[^,]*")))
+    cov$coverage_alt<-ifelse(cov$na_count==2, as.numeric(str_extract(cov[,4],"(?<=,).*")),
+                             as.numeric(str_extract(cov[,2],"(?<=,).*")))
+  }
   
   return(data.frame(cov[,c("coverage","coverage_ref","coverage_alt","genotype")]))
 }
